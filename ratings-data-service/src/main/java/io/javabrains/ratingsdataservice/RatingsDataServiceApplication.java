3eaea51e95efa398e5107935a166a00263a1e430
Rahul <rahtiwari@gmail.com>
2020-02-21 23:28:30 +0530

    commiting for GitKrakken test

--- ratings-data-service/src/main/java/io/javabrains/ratingsdataservice/RatingsDataServiceApplication.java
+++ ratings-data-service/src/main/java/io/javabrains/ratingsdataservice/RatingsDataServiceApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RatingsDataServiceApplication {
 
 	public static void main(String[] args) {
+		System.out.println("adding GitKrakken Comment for test");
+		System.out.println("it is working yes!!!!");
 		SpringApplication.run(RatingsDataServiceApplication.class, args);
 	}
 
